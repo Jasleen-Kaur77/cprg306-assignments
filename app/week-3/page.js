d9f1da4ac2731390cf26ee13259b306d80f9996f
Jasleen-Kaur77 <jasleenkaur1@edu.sait.ca>
2025-09-24 12:14:38 -0600

    Update page.js

---
+++ app/week-3/page.js
@@ -0,0 +1,14 @@
+import ItemList from "./item-list";
+
+export default function Page() {
+  return (
+    <main>
+      <header>
+        <h1>Shopping List</h1>
+      </header>
+      <section>
+        <ItemList />
+      </section>
+    </main>
+  );
+}
