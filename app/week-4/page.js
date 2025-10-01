c84280a2db457c65f0c831ac03545b847ab8a88b
Jasleen-Kaur77 <jasleenkaur1@edu.sait.ca>
2025-10-01 01:23:08 -0600

    Create and edit page.js and new-item.js

---
+++ app/week-4/page.js
@@ -0,0 +1,12 @@
+import NewItem from "./new-item";
+
+export default function Page() {
+  return (
+    <main>
+      <h1> Add New Item</h1>
+      <section>
+        <NewItem />
+      </section>
+    </main>
+  );
+}
