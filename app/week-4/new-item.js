292bf7e41022d0aca8ffb18a8ea8b9d12b80108c
Jasleen-Kaur77 <jasleenkaur1@edu.sait.ca>
2025-10-01 01:44:53 -0600

    Create increment and decrement functions

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -4,5 +4,14 @@ import { useState } from "react";
 
 export default function NewItem() {
   const [quantity, setQuantity] = useState(1);
+
+  const increment = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const decrement = () => {
+    setQuantity(quantity - 1);
+  };
+
   // return();
 }
