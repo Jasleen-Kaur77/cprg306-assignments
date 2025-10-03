41ade814f559b2103f6c9e5cbc564e19b9498dda
Jasleen-Kaur77 <jasleenkaur1@edu.sait.ca>
2025-10-03 03:57:56 -0600

    Correct Indentation

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -21,12 +21,8 @@ export default function NewItem() {
     <div className="shadow-2xl border border-pink-800 bg-pink-100 rounded-md p-4 my-4 mx-auto max-w-xl">
       <p className="text-xl font-semibold text-pink-600">Quantity: {quantity}</p>
       <div className="flex justify-items-start gap-4 mb-4">
-
-
         <button onClick={decrement} disabled={quantity === 1} className="bg-pink-500 hover:bg-pink-700 disabled:bg-pink-300 rounded m-3 p-3 text-white w-12 font-bold text-2xl"> - </button>
         <button onClick={increment} disabled={quantity === 20} className="bg-pink-500 hover:bg-pink-700 disabled:bg-pink-300 rounded m-3 p-3 text-white w-12 font-bold text-2xl"> + </button>
-
-
       </div>
       <p className="text-pink-400">Allowed range 1-20</p>
     </div>
