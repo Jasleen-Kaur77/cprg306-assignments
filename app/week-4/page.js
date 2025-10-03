289b76638e5b2eceba68bbb425b58ab212550a6d
Jasleen-Kaur77 <jasleenkaur1@edu.sait.ca>
2025-10-03 03:55:38 -0600

    Add styling using Tailwind CSS and disable feature to buttons

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -3,7 +3,7 @@ import NewItem from "./new-item";
 export default function Page() {
   return (
     <main>
-      <h1> Add New Item</h1>
+      <h1 className="text-2xl lg:text-3xl font-bold text-center text-pink-900 m-6" > Add New Item</h1>
       <section>
         <NewItem />
       </section>
