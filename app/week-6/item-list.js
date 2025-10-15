9829a19fbd348233aa04e881901f103c2a2f9f41
Jasleen-Kaur77 <jasleenkaur1@edu.sait.ca>
2025-10-15 01:33:35 -0600

    Create sort buttons

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -16,20 +16,11 @@ export default function ItemList() {
 
 
   return (
-    <ul>
-      <Item {...items[0]} />
-      <Item {...items[1]} />
-      <Item {...items[2]} />
-      <Item {...items[3]} />
-      <Item {...items[4]} />
-      <Item {...items[5]} />
-      <Item {...items[6]} />
-      <Item {...items[7]} />
-      <Item {...items[8]} />
-      <Item {...items[9]} />
-      <Item {...items[10]} />
-      <Item {...items[11]} />
-    </ul>
+    <div>
+      <p>Sort by:</p>
+      <button onClick={() => setSortBy("name")}>Name</button>
+      <button onClick={() => setSortBy("category")}>Category</button>
+    </div>
   );
 
 }
