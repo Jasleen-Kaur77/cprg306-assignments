d399ca6833d70d841278ab330056ee0a5e51606d
Jasleen-Kaur77 <jasleenkaur1@edu.sait.ca>
2025-10-24 01:35:51 -0600

    Modify page.js

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,13 +1,23 @@
+"use client";
+
 import ItemList from "./item-list";
+import NewItem from "./new-item";
+import itemsData from "./items.json";
 
 export default function Page() {
+  const [items, setItems] = useState(itemsData);
+  const handleAddItem = (newItem) => {
+    setItems([...items, newItem]);
+  };
+
   return (
     <main>
       <header className="m-4 p-5 bg-blue-200 mx-auto max-w-2xl rounded-xl">
         <h1 className="text-2xl lg:text-4xl font-bold text-center text-blue-950" >Shopping List</h1>
       </header>
       <section>
-        <ItemList />
+        <NewItem onAddItem={handleAddItem} />
+        <ItemList items={items} />
       </section>
     </main>
   );
