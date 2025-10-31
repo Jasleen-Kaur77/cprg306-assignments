832729940af640641a2b37681645217fd44c4728
Jasleen-Kaur77 <jasleenkaur1@edu.sait.ca>
2025-10-31 00:12:29 -0600

    Update item.js

--- app/week-8/item.js
+++ app/week-8/item.js
@@ -1,6 +1,7 @@
-export default function Item({ name, quantity, category }) {
+export default function Item({ name, quantity, category, onSelect }) {
   return (
-    <li className="shadow-lg text-left text-xl my-4 mx-auto max-w-2xl border border-blue-800 bg-blue-100 rounded-md p-4">
+    <li onClick={onSelect}
+      className="shadow-lg text-left text-xl my-4 mx-auto max-w-2xl border border-blue-800 bg-blue-100 rounded-md p-4">
       <p className="font-semibold">{name}</p>
       <p>Quantity: {quantity}</p>
       <p>Category: {category}</p>
