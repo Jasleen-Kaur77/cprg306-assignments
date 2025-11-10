251904b276c1ba5e0d78bc80a7ad108bb747a083
Jasleen-Kaur77 <jasleenkaur1@edu.sait.ca>
2025-11-10 02:49:17 -0600

    Updated page.js

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -1,1 +1,45 @@
-//landing page
+"use client";
+
+// Import the useUserAuth hook
+import { useUserAuth } from "../contexts/AuthContext";
+import Link from "next/link";
+
+export default function Page() {
+  // Use the useUserAuth hook to get the user object and the login and logout functions
+  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+  const handleLogin = async () => {
+    try {
+      await gitHubSignIn();
+    } catch (error) {
+      console.error("Login error: ", error);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      await firebaseSignOut();
+    } catch (error) {
+      console.error("Logout failed: ", error);
+    }
+  };
+
+  return (
+    <main>
+      {user ? (
+        <div>
+          <p>
+            Welcome, {user.displayName} ({user.email})
+          </p>
+          <button onClick={handleLogout}>
+            Log Out
+          </button>
+          <Link href="/week-9/shopping-list"> Go To Shopping List</Link>
+        </div>
+      ) : (
+        <button onClick={handleLogin}>Login</button>
+      )}
+    </main>
+  );
+
+}
