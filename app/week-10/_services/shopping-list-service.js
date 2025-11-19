55a1b8168fed6f03d94ab2a6e426c1eff7a0af8f
Jasleen-Kaur77 <jasleenkaur1@edu.sait.ca>
2025-11-18 18:07:23 -0600

    Update shopping-list-service.js

--- app/week-10/_services/shopping-list-service.js
+++ app/week-10/_services/shopping-list-service.js
@@ -1,2 +1,35 @@
 import { db } from "../_utils/firebase";
 import { collection, getDocs, addDoc, query } from "firebase/firestore";
+
+export const getItems = async (userId) => {
+  try {
+    const ref = collection(db, "users", userId, "items");
+    const q = query(ref);
+    const querySnapshot = await getDocs(q);
+
+    const items = [];
+
+    querySnapshot.docs.forEach((doc) => {
+      items.push({
+        id: doc.id,
+        ...doc.data(),
+      });
+    });
+
+    return items;
+  }
+  catch (error) {
+    console.error(`Error reading collection: ${userId}`, error);
+  }
+};
+
+export const addItem = async (userId, item) => {
+  try {
+    const ref = collection(db, "users", userId, "items");
+    const docRef = await addDoc(ref, item);
+    return docRef.id;
+  }
+  catch (error) {
+    console.error(`Error adding item for user: ${userId}`, error);
+  }
+};
